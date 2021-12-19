bf0538e8d47566126ca0786596becdb3f060146b
TacticDevs <jamiesaidi@outlook.com>
2021-12-19 21:00:50 +0200

    Version 1.0

---
+++ NavButton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PageNavigation
+{
+
+    public class NavButton : ButtonBase
+    {
+        static NavButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavButton), new FrameworkPropertyMetadata(typeof(NavButton)));
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(NavButton), new PropertyMetadata(null));
+        public static readonly DependencyProperty TextProperty =DependencyProperty.Register("Text", typeof(string), typeof(NavButton), new PropertyMetadata(null));
+        public static readonly DependencyProperty NavUriProperty = DependencyProperty.Register("NavUri", typeof(Uri), typeof(NavButton), new PropertyMetadata(null));
+
+
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public Uri NavUri
+        {
+            get { return (Uri)GetValue(NavUriProperty); }
+            set { SetValue(NavUriProperty, value); }
+        }
+
+
+
+
+    }
+}
